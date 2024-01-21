15eda22fc2ae6dae8a828f2d3d079cefb75a5063
Affaq-Ahmed <aaawan165@gmail.com>
2024-01-21 13:15:45 +0500

    Add axios library and fetch advice from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
-	return <div>App</div>;
+	const [advice, setAdvice] = useState("");
+
+	useEffect(() => {
+		const fetchAdvice = async () => {
+			const response = await fetch("https://api.adviceslip.com/advice");
+			const data = await response.json();
+			console.log(data);
+			setAdvice(data.slip.advice);
+		};
+
+		fetchAdvice();
+	}, []);
+
+	return (
+		<div className="app">
+			<div className="card">
+				<h1 className="heading">{advice}</h1>
+				<button className="button" onClick={() => window.location.reload()}>
+					<span>GIVE ME ADVICE!</span>
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default App;
